9ab5a5a27b8882eae118e82259f37ab305ee2fef
oyzq_glb <ouyangzhiqiang98_glb@gmail.com>
2019-06-03 13:48:53 +0800

    weichat

---
+++ 01-demo/pages/index3/index3.js
@@ -0,0 +1,73 @@
+// pages/index3/index3.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg:"你好啊!",
+    num:1000,
+    isBoy:true,
+    person:{
+      name:"小爱同学",
+      height:"188"
+    },
+    title:"这个是新页面index3"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ 01-demo/pages/index4/index4.js
@@ -0,0 +1,71 @@
+// pages/index4/index4.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isChecked:true,
+    num1:10,
+    num2:1000,
+    num3:2,
+    str1:"hello",
+    str2:"world !"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
